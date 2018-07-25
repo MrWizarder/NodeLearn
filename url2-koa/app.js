2bf4ba9b43c12e592714f5c51ce87d48dc9a6bc8
lw <>
2018-07-25 09:45:10 +0800

    koa2

--- url2-koa/app.js
+++ url2-koa/app.js
@@ -2,6 +2,12 @@ const Koa = require('koa');
 const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser');
 const app = new Koa();
+const fs = require('fs');
+const controller = require('./controller');
+
+app.use(bodyParser());
+app.use(controller());
+
 
 app.use(async (ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
@@ -13,30 +19,30 @@ router.get('/hello/:name', async (ctx, next) => {
     ctx.response.body = `<h1>Hello, ${name}!</h1>`;
 });
 
-router.get('/', async (ctx, next) => {
-    ctx.response.body = `<h1>Index</h1>
-        <form action="/signin" method="post">
-            <p>Name: <input name="name" value="koa"></p>
-            <p>Password: <input name="password" type="password"></p>
-            <p><input type="submit" value="submit"></p>
-        </form>`;
-});
+// router.get('/', async (ctx, next) => {
+//     ctx.response.body = `<h1>Index</h1>
+//         <form action="/signin" method="post">
+//             <p>Name: <input name="name" value="koa"></p>
+//             <p>Password: <input name="password" type="password"></p>
+//             <p><input type="submit" value="submit"></p>
+//         </form>`;
+// });
 
-router.post('/signin', async (ctx, next) => {
-    var
-        name = ctx.request.body.name || '',
-        password = ctx.request.body.password || '';
-    console.log(`signin with name: ${name}, password: ${password}`);
-    if (name === 'koa' && password === '12345') {
-        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
-    } else {
-        ctx.response.body = `<h1>Login failed!</h1>
-        <p><a href="/">Try again</a></p>`;
-    }
-})
+// router.post('/signin', async (ctx, next) => {
+//     var
+//         name = ctx.request.body.name || '',
+//         password = ctx.request.body.password || '';
+//     console.log(`signin with name: ${name}, password: ${password}`);
+//     if (name === 'koa' && password === '12345') {
+//         ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+//     } else {
+//         ctx.response.body = `<h1>Login failed!</h1>
+//         <p><a href="/">Try again</a></p>`;
+//     }
+// })
 
 
-app.use(bodyParser());
-app.use(router.routes());
+
+
 app.listen(3000);
 console.log('app started at port 3000...');
